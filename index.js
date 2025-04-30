a4161b2fbbf83d57b96077f486590b056bc35df5
SarahBiscuit <scfletcher2000@hotmail.com>
2025-04-30 21:45:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,23 +13,18 @@ app.use(express.static("public"));
 //app.get to render webpage and send the relevant API details to the index.ejs file.//
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get("https://anapioficeandfire.com/api/characters/");
+        const randomId = Math.floor(Math.random() * 2138) + 1;
+        const response = await axios.get(`https://anapioficeandfire.com/api/characters/${randomId}`);
 
-        // Pick a random character from the list of URLs
-    const characters = response.data;
-    const randomUrl = characters[Math.floor(Math.random() * characters.length)];
+        const character = response.data;
 
-    // Fetch detailed data for the selected random character
-    const character = await axios.get(randomUrl);
-
-    // Render the data to the view
-    res.render("index.ejs", {
-      name: character.data.name,
-      gender: character.data.gender,
-      DOB: character.data.born,
-      died: character.data.died,
-      nicknames: character.data.aliases
-    });
+        res.render("index.ejs", {
+            name: character.name || " ",
+            gender: character.gender || " ",
+            DOB: character.born || " ",
+            died: character.died || " ",
+            nicknames: character.aliases.length > 0 ? character.aliases : ["None"]
+          });
 
 } catch (error) {
     console.error("API fetch failed:", error.message);
