9829b4254c9b31329d4102d192630b9f8de32063
SarahBiscuit <scfletcher2000@hotmail.com>
2025-04-30 20:28:32 +0100

    Base Index.js frame created
    Have created the base frame of the index.ejs, without the HTTP routes yet.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));  
+
+//app.get to render webpage and pull in the api details.//
+
+//app.post to pull in the user form input and send it to the index.ejs file so that it can display on the screen//
+//When a user presses the generate new character button, I need their form post to clear as well.//
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000.");
+    });
