a859e80c6c28b2ca52569ad8bafbd6df54b672af
SarahBiscuit <scfletcher2000@hotmail.com>
2025-04-30 21:39:29 +0100

    Working on app.get route
    Working on app.get route to generate random character from the API

--- index.js
+++ index.js
@@ -10,10 +10,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));  
 
-//app.get to render webpage and pull in the api details.//
-//Renders the form//
-app.get("/", (req, res) => {
-    res.render("index.ejs",);
+//app.get to render webpage and send the relevant API details to the index.ejs file.//
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://anapioficeandfire.com/api/characters/");
+
+        // Pick a random character from the list of URLs
+    const characters = response.data;
+    const randomUrl = characters[Math.floor(Math.random() * characters.length)];
+
+    // Fetch detailed data for the selected random character
+    const character = await axios.get(randomUrl);
+
+    // Render the data to the view
+    res.render("index.ejs", {
+      name: character.data.name,
+      gender: character.data.gender,
+      DOB: character.data.born,
+      died: character.data.died,
+      nicknames: character.data.aliases
+    });
+
+} catch (error) {
+    console.error("API fetch failed:", error.message);
+    res.status(500).send("Something went wrong.");
+  }
 });
 
 //app.post to pull in the user form input and send it to the index.ejs file so that it can display on the screen//
