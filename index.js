793de226bdab555dfea316692b09282e7fb1c3b2
SarahBiscuit <scfletcher2000@hotmail.com>
2025-04-30 20:49:05 +0100

    Header created with website title.  Ejs installed.
    Header created

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));  
 
 //app.get to render webpage and pull in the api details.//
+//Renders the form//
+app.get("/", (req, res) => {
+    res.render("index.ejs",);
+});
 
 //app.post to pull in the user form input and send it to the index.ejs file so that it can display on the screen//
 //When a user presses the generate new character button, I need their form post to clear as well.//
